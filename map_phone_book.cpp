62e6fafa08af389f0c56b3cb05f9ba06e1b30931
AnahitStepanyan <74183824+AnahitStepanyan@users.noreply.github.com>
2020-12-24 22:06:13 +0400

    Create map_phone_book.cpp

---
+++ map_phone_book.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+int main() {
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  std::map <std::string, std::string> phoneBook;
+  int n; std::cin >> n;
+  int tmpN = n;
+  while(tmpN--){
+    std::string name, phoneNumber;
+    std::cin >> name >> phoneNumber;
+    phoneBook.insert(std::pair<std::string, std::string>(name, phoneNumber));
+    // phoneBook[name] = phoneNumber;
+  }
+  std::string key;
+  while(std::cin >> key){
+    if(phoneBook.find(key) != phoneBook.end()){
+      std::cout << key << "=" << phoneBook.find(key)->second << std::endl;
+    } else {
+      std::cout << "Not found" << std::endl;
+    }
+  }
+  
+  return 0;
+}
